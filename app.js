87900f0aef944f2996b97d82f794f3c084565275
joeecasa <joeecasa@gmail.com>
2022-04-20 20:55:28 -0300

    start

--- app.js
+++ app.js
@@ -8,19 +8,19 @@ app.use(express.static("public"));
 
 // app.use("/static",express.static(__dirname,))
 
-app.get ("/",(req, res) => {
+app.get("/",(req, res) =>{
     res.sendFile(path.join(__dirname,"/views/home.html"))
 })
 
-app.get ("/registro",(req, res) => {
+app.get("/registro",(req, res) =>{
     res.sendFile(path.join(__dirname,"/views/register.html"))
 })
 
-app.get ("/login",(req, res) => {
+app.get("/login",(req, res) =>{
     res.sendFile(path.join(__dirname,"/views/login.html"))
 })
 
-app.post ("/",(req, res) => {
+app.post("/",(req, res) =>{
     res.sendFile(path.join(__dirname,"/views/home.html"))
 })
 
